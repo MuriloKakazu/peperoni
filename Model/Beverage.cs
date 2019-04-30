383356fc17aa7e3cec21848ef648f8fe2a1528dd
Murilo Kakazu <yuri.kakazu@gmail.com>
2019-04-29 23:22:15 -0300

    introducing model layer

---
+++ Model/Beverage.cs
@@ -0,0 +1,31 @@
+namespace Model {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Beverage")]
+    public partial class Beverage {
+        [StringLength(36)]
+        public string Id { get; set; }
+
+        [Required]
+        [StringLength(36)]
+        public string OrderId { get; set; }
+
+        [Required]
+        [StringLength(36)]
+        public string ProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal UnitPrice { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal TotalPrice { get; set; }
+
+        public virtual Product Product { get; set; }
+    }
+}
