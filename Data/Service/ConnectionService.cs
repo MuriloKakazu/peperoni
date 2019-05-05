edbd47e203a60f371182106ae32e5ab7f88930a7
Murilo Kakazu <yuri.kakazu@gmail.com>
2019-05-04 21:59:54 -0300

    added connection service and encryption

---
+++ Data/Service/ConnectionService.cs
@@ -0,0 +1,40 @@
+﻿using Data.Encryption;
+using Data.Repository;
+using System;
+
+namespace Data.Service {
+    public class ConnectionService {
+       private AesRepository AesRepository { get; set; }
+       private ConnectionStringRepository ConnectionStringRepository { get; set; }
+
+        public ConnectionService() {
+            AesRepository = new AesRepository();
+            ConnectionStringRepository = new ConnectionStringRepository();
+        }
+
+        public string GetConnectionStringForContext(string context) {
+            try {
+
+                var aes = AesRepository.GetAes(context);
+                var encrypted = ConnectionStringRepository.GetConnectionString(context);
+                var decrypted = Decrypt.Base64(encrypted).WithAes(aes);
+
+                return decrypted;
+
+            } catch (AesRepository.AesRepositoryException e) {
+                throw new ConnectionServiceException($"Error retrieving aes: {e.Message}");
+            } catch (ConnectionStringRepository.ConnectionStringRepositoryException e) {
+                throw new ConnectionServiceException($"Error retrieving connection string: {e.Message}");
+            } catch (Exception e) {
+                throw new ConnectionServiceException(
+                    $"Unknown error when attempting to retrieve connection " +
+                    $"string for context '{context}': {e.Message}");
+            }
+        }
+
+        public class ConnectionServiceException : Exception {
+            public ConnectionServiceException(string message) 
+                : base(message) { }
+        }
+    }
+}
