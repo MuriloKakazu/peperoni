69d134eca8af7fff4132ae98051b001291431fa9
mupapi <mupapi@hotmail.com>
2019-05-28 00:24:33 -0300

    OrderCreation finished

---
+++ Presentation/Components/Windows/BeverageCreationPopup.xaml.cs
@@ -0,0 +1,125 @@
+﻿using Domain.Builder;
+using Domain.Model.PizzaShop;
+using Infrastructure.Exceptions;
+using Infrastructure.Rule;
+using Presentation.Controllers;
+using Presentation.Util;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Presentation.Components.Windows {
+    /// <summary>
+    /// Interaction logic for BeverageCreationPopup.xaml
+    /// </summary>
+    /// OrderBuilder orderBuilder { get; set; }
+    public partial class BeverageCreationPopup : Window {
+        OrderBuilder orderBuilder { get; set; }
+        BeverageController controller { get; set; }
+        BeverageBuilder builder { get; set; }
+        ObservableCollection<Beverage> beverages { get; set; }
+
+        public BeverageCreationPopup(OrderBuilder orderBuilder) : this() {
+            this.orderBuilder = orderBuilder;
+            InitializeComponent();
+
+            BeverageListView.ItemsSource = beverages;
+            txtQuantity.Text = "1";
+            cbProduct.ItemsSource = controller.GetAvailableDrinks();
+            cbProduct.DisplayMemberPath = "Name";
+        }
+
+        public BeverageCreationPopup() {
+            this.controller = new BeverageController();
+            this.builder = new BeverageBuilder();
+            this.beverages = new ObservableCollection<Beverage>();
+        }
+
+        private void RefreshBeveragePrice() {
+            Beverage beverage = builder.Build();
+            if(String.IsNullOrEmpty(beverage.ProductId) || beverage.Quantity == 0) {
+                BeveragePrice.Content = "-";
+            } else {
+                BeveragePrice.Content = $"{beverage.TotalPrice:C}";
+            }
+        }
+
+        private void UpdateProduct(object sender, SelectionChangedEventArgs e) {
+            builder.WithProduct((Product)cbProduct.SelectedItem);
+            RefreshBeveragePrice();
+        }
+
+        private void UpdateQuantity(object sender, TextChangedEventArgs e) {
+            if(!String.IsNullOrEmpty(txtQuantity.Text)) {
+                try {
+                    new Validator<object>(this)
+                        .Ensure(InputValidator.IsOverZero(txtQuantity.Text), "A quantidade deve ser um número válido maior que zero!")
+                        .Run();
+
+                    builder.WithQuantity(Int32.Parse(txtQuantity.Text));
+                    RefreshBeveragePrice();
+                } catch(ValidationException exc) {
+                    MessageUtils.ShowError(exc.Message);
+                    txtQuantity.Text = "1";
+                }
+            }
+        }
+        private void validateQuantity(object sender, RoutedEventArgs e) {
+            try {
+                new Validator<object>(this)
+                    .Ensure(InputValidator.IsOverZero(txtQuantity.Text), "A quantidade deve ser um número válido maoior que zero!")
+                    .Run();
+            } catch(ValidationException exc) {
+                MessageUtils.ShowError(exc.Message);
+                txtQuantity.Text = "1";
+                builder.WithQuantity(Int32.Parse(txtQuantity.Text));
+                RefreshBeveragePrice();
+            }
+        }
+
+        private void AddBeverage(object sender, RoutedEventArgs e) {
+            Beverage beverage = builder.Build();
+            if(AlreadyAddedSimilar(beverage)) {
+                MessageUtils.ShowError($"O acompanhamento {beverage} já foi adicionado ao pedido");
+            } else {
+                beverages.Add(builder.Build());
+                builder = new BeverageBuilder().WithQuantity(Int32.Parse(txtQuantity.Text));
+                ResetForm();
+            }
+        }
+
+        private bool AlreadyAddedSimilar(Beverage addedBeverage) {
+            return beverages.ToList().Any(p =>
+                p.IsSimilarTo(addedBeverage)
+            );
+        }
+
+        private void ResetForm() {
+            ClearInputs();
+        }
+
+        private void ClearInputs() {
+            cbProduct.SelectedIndex = -1;
+            txtQuantity.Text = "1";
+        }
+        private void CommitAndClose(object sender, RoutedEventArgs e) {
+            orderBuilder.WithBeverages(beverages);
+            this.Close();
+        }
+
+        private void Close(object sender, RoutedEventArgs e) {
+            this.Close();
+        }
+    }
+}
--- Presentation/Pages/PizzaCreationPopup.xaml.cs
+++ Presentation/Pages/PizzaCreationPopup.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using Domain.Builder;
 using Domain.Model.PizzaShop;
+using Infrastructure.Exceptions;
+using Infrastructure.Rule;
 using Presentation.Controllers;
+using Presentation.Util;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,13 +24,16 @@ namespace Presentation.Pages {
     /// Interaction logic for PizzaCreationPopup.xaml
     /// </summary>
     public partial class PizzaCreationPopup : Window {
-        OrderBuilder order { get; set; }
+        OrderBuilder orderBuilder { get; set; }
         PizzaController controller { get; set; }
         PizzaBuilder builder { get; set; }
+        ObservableCollection<Pizza> pizzas { get; set; }
         public PizzaCreationPopup(OrderBuilder order) : this() {
-            this.order = order;
+            this.orderBuilder = order;
             InitializeComponent();
 
+            PizzaListView.ItemsSource = pizzas;
+            txtQuantity.Text = "1";
             cbFirstTopping.ItemsSource = cbSecondTopping.ItemsSource = controller.GetAvailableToppings();
             cbBorder.ItemsSource = controller.GetAvailableBorders();
             cbBorder.DisplayMemberPath = cbFirstTopping.DisplayMemberPath = cbSecondTopping.DisplayMemberPath = "Name";
@@ -35,30 +42,98 @@ namespace Presentation.Pages {
         protected PizzaCreationPopup() {
             this.controller = new PizzaController();
             this.builder = new PizzaBuilder();
+            this.pizzas = new ObservableCollection<Pizza>();
         }
 
-        private void refreshPizzaPrice() {
+        private void RefreshPizzaPrice() {
             Pizza pizza = builder.Build();
             if(String.IsNullOrEmpty(pizza.BorderId) || String.IsNullOrEmpty(pizza.FirstToppingId)) {
                 PizzaPrice.Content = "-";
             } else {
-                PizzaPrice.Content = $"R$ {pizza.TotalPrice:C}";
+                PizzaPrice.Content = $"{pizza.TotalPrice:C}";
             }
         }
 
-        private void cbBorder_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+        private void UpdateBorder(object sender, SelectionChangedEventArgs e) {
             builder.WithBorder((Product)cbBorder.SelectedItem);
-            refreshPizzaPrice();
+            RefreshPizzaPrice();
         }
 
-        private void cbSecondTopping_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+        private void UpdateSecondTopping(object sender, SelectionChangedEventArgs e) {
             builder.WithSecondTopping((Product)cbSecondTopping.SelectedItem);
-            refreshPizzaPrice();
+            RefreshPizzaPrice();
         }
 
-        private void cbFirstTopping_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+        private void updateFirstTopping(object sender, SelectionChangedEventArgs e) {
             builder.WithFirstTopping((Product)cbFirstTopping.SelectedItem);
-            refreshPizzaPrice();
+            RefreshPizzaPrice();
+        }
+
+        private void UpdateQuantity(object sender, TextChangedEventArgs e) {
+            if(!String.IsNullOrEmpty(txtQuantity.Text)) {
+                try {
+                    new Validator<object>(this)
+                        .Ensure(InputValidator.IsOverZero(txtQuantity.Text), "A quantidade deve ser um número válido maior que zero!")
+                        .Run();
+
+                    builder.WithQuantity(Int32.Parse(txtQuantity.Text));
+                    RefreshPizzaPrice();
+                } catch(ValidationException exc) {
+                    MessageUtils.ShowError(exc.Message);
+                    txtQuantity.Text = "1";
+                }
+            }
+        }
+
+        private void validateQuantity(object sender, RoutedEventArgs e) {
+            try {
+                new Validator<object>(this)
+                    .Ensure(InputValidator.IsOverZero(txtQuantity.Text), "A quantidade deve ser um número válido maoior que zero!")
+                    .Run();
+            } catch(ValidationException exc) {
+                MessageUtils.ShowError(exc.Message);
+                txtQuantity.Text = "1";
+                builder.WithQuantity(Int32.Parse(txtQuantity.Text));
+                RefreshPizzaPrice();
+            }
+        }
+
+        private void AddPizza(object sender, RoutedEventArgs e) {
+            Pizza pizza = builder.Build();
+            if(AlreadyAddedSimilar(pizza)) {
+                MessageUtils.ShowError($"A pizza {pizza} já foi adicionada ao pedido");
+            } else {
+                pizzas.Add(builder.Build());
+                builder = new PizzaBuilder().WithQuantity(Int32.Parse(txtQuantity.Text));
+                ResetForm();
+            }
+        }
+
+        private bool AlreadyAddedSimilar(Pizza addedPizza) {
+            return pizzas.ToList().Any(p => 
+                p.IsSimilarTo(addedPizza)
+            );
+        }
+
+        private void ResetForm() {
+            
+            ClearInputs();
+        }
+
+        private void ClearInputs() {
+            cbBorder.SelectedIndex = -1;
+            cbFirstTopping.SelectedIndex = -1;
+            cbSecondTopping.SelectedIndex = -1;
+            txtQuantity.Text = "1";
+        }
+
+        private void CommitAndClose(object sender, RoutedEventArgs e) {
+            orderBuilder.WithPizzas(pizzas);
+            this.Close();
+        }
+
+        private void Close(object sender, RoutedEventArgs e) {
+            this.Close();
         }
     }
 }
